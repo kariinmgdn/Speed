796e5d4c26b3d7a557d3158361e0aa958ea69cca
Karīna Dzene <karina.dzene@gmail.com>
2023-05-03 12:35:58 +0300

    added ResponseStatusException

--- back-end/src/main/java/com/example/backend/controller/SpeedDataController.java
+++ back-end/src/main/java/com/example/backend/controller/SpeedDataController.java
@@ -3,12 +3,15 @@ package com.example.backend.controller;
 import com.example.backend.domain.AverageSpeed;
 import com.example.backend.domain.SpeedData;
 import com.example.backend.service.SpeedDataService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/speed")
@@ -24,11 +27,17 @@ public class SpeedDataController {
                                         @RequestParam String to,
                                         @RequestParam String speed,
                                         @RequestParam String page) throws Exception {
+        if (Objects.equals(page, "")) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
         return service.getData(from, to, speed, page);
     }
 
     @GetMapping("/average")
     public List<AverageSpeed> getAverageSpeed(@RequestParam String date) {
+        if (Objects.equals(date, "")) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
         return service.getAverageSpeed(date);
     }
 }
